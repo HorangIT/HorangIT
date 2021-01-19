32fca32a869d272aa7d57c8a8c949ba885588086
이승아 <dltmddk1023@gmail.com>
2021-01-19 17:18:26 +0900

    [Chore] Add Style Template

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,24 +1,61 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/components/Home.vue";
+import Blog from '@/components/Blog.vue';
+import Layout from '@/components/Layout.vue';
+import Shop from '@/components/Shop.vue';
+import Product from '@/components/Product.vue';
+import Post from '@/components/Post.vue';
+import Cart from '@/components/Cart.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    component: Layout,
+    children:[
+      {
+        path:'/',
+        component:Home,
+        name:'Home'
+      },
+      {
+        path:'/shop',
+        component:Shop,
+        name:'Shop'
+      },
+      {
+        path:'/product',
+        component:Product,
+        name:'Product'
+      },
+      {
+        path:'/blog',
+        component:Blog,
+        name:'Blog'
+      },
+      {
+        path:'/post',
+        component:Post,
+        name:'Post'
+      },
+      {
+        path:'/cart',
+        component:Cart,
+        name:'Cart'
+      }
+    ]
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = new VueRouter({
