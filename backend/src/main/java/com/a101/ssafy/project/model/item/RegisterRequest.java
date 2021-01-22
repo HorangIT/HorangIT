605ef48758bd3e32c668cc2f242270b44d06fbb3
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-01-22 15:42:03 +0900

    [Feat] setUp RegisterRequest

--- backend/src/main/java/com/a101/ssafy/project/model/item/RegisterRequest.java
+++ backend/src/main/java/com/a101/ssafy/project/model/item/RegisterRequest.java
@@ -1,11 +1,49 @@
 package com.a101.ssafy.project.model.item;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.ToString;
 
 @Valid
 @ToString
 public class RegisterRequest {
+	@ApiModelProperty(required = true)
+	@NotNull
+	String title;
+	
+	@ApiModelProperty(required = true)
+	@NotNull
+	String description;
+	
+	@ApiModelProperty(required = true)
+	@NotNull
+	String startPrice;
+	
+	@ApiModelProperty(required = true)
+	@NotNull
+	String happyPrice;
+	
+	@ApiModelProperty(required = true)
+	String grade;
+	
+	@ApiModelProperty(required = true)
+	String direct;
+	
+	@ApiModelProperty(required = true)
+	String startDate;
+	
+	@ApiModelProperty(required = true)
+	String endDate;
+	
+	@ApiModelProperty(required = true)
+	String category;
+	
+	//@ApiModelProperty(required = true)
+	String[] image = new String[3];
+	
+	@ApiModelProperty(required = true)
+	String userId;
 
 }
