a5e43f3abeb27277cb71f78c415636dc59d7e930
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-01-22 15:36:06 +0900

    [Add] JAVAX validation Dependency in pom.xml

--- backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
@@ -2,15 +2,18 @@ package com.a101.ssafy.project.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.a101.ssafy.project.model.item.RegisterRequest;
 
 @CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/item")
 public class ItemController {
 	@PostMapping
-	public Object registerItem() {
+	public Object registerItem(@RequestBody RegisterRequest request) {
 		return null;
 	}
 	
---
+++ backend/src/main/java/com/a101/ssafy/project/model/item/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.a101.ssafy.project.model.item;
+
+import javax.validation.Valid;
+
+import lombok.ToString;
+
+@Valid
+@ToString
+public class RegisterRequest {
+
+}
