2a374a07a6d7e31d861dcdb6e3b27dfe950b932e
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-01-22 15:25:12 +0900

    [Add] JPARepository Dependency in pom.xml

--- backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
@@ -1,9 +1,11 @@
 package com.a101.ssafy.project.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/item")
 public class ItemController {
