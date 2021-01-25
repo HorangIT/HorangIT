13b60cda20331fb8f97fa9bbe19ec17f37ea7c02
sungjin <tjdwls3469@gmail.com>
2021-01-25 17:33:37 +0900

    [Feat] Update Login_BE And Add Signup_BE   - 로그인 서비스 컨트롤러 서비스 분리   - 회원가입 구현

--- backend/src/main/java/com/a101/ssafy/project/controller/UserController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/UserController.java
@@ -1,25 +1,18 @@
 package com.a101.ssafy.project.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.a101.ssafy.project.dao.UserRepository;
-import com.a101.ssafy.project.jwt.TokenProvider;
 import com.a101.ssafy.project.model.BasicResponse;
 import com.a101.ssafy.project.model.user.LoginDto;
-import com.a101.ssafy.project.model.user.TokenDto;
-import com.a101.ssafy.project.model.user.User;
+import com.a101.ssafy.project.model.user.SignupDto;
+import com.a101.ssafy.project.service.UserServiceImpl;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -36,43 +29,17 @@ import io.swagger.annotations.ApiResponses;
 public class UserController {
 	
     @Autowired
-    UserRepository userDao;
-    
-    @Autowired
-    PasswordEncoder passwordEncoder;
-    
-	private final TokenProvider tokenProvider;
-	private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
-	public UserController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
-		this.tokenProvider = tokenProvider;
-		this.authenticationManagerBuilder = authenticationManagerBuilder;
-	}
+    UserServiceImpl userService;
 
 	@ApiOperation(value = "로그인")
 	@PostMapping("/login")
 	public Object login(@RequestBody LoginDto loginDto) {
-		final BasicResponse result = new BasicResponse();
-		User user = userDao.findByEmail(loginDto.getEmail());
-		
-		if(user == null || !passwordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
-			result.status = false;
-	        result.data = "로그인 실패";
-	        return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
-		}
-		
-		// 이메일과, 비밀번호를 통해서 authenticationToken 생성
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
-
-		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-
-		// 인증정보를 통해서 JWT 토큰생성
-		String jwt = tokenProvider.createToken(authentication);
-        
-        result.status = true;
-        result.data = "로그인 성공";
-        result.object = new TokenDto(jwt);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+		return userService.login(loginDto);
+	}
+	
+	@ApiOperation(value = "회원가입")
+	@PostMapping("/signup")
+	public Object signup(@Valid @RequestBody SignupDto signupDto) {
+		return userService.signup(signupDto);
 	}
 }
