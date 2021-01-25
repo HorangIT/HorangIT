a5cca546917e4a5768c61a8a43345f8002f9f327
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-01-25 14:22:56 +0900

    [Add] ImageController & s3Service

--- backend/src/main/java/com/a101/ssafy/project/controller/ImageController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/ImageController.java
@@ -1,5 +1,11 @@
 package com.a101.ssafy.project.controller;
 
+import org.springframework.stereotype.Controller;
+
+import com.a101.ssafy.project.service.S3Service;
+
+@Controller
 public class ImageController {
+	private S3Service s3Service;
 
 }
