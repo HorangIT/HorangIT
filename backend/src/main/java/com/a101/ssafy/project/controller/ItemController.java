e162198e85993523092319848530c79d5a38637c
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-01-26 13:11:42 +0900

    [Feat] Insert Image Url(S3) in Image table with FK

--- backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.a101.ssafy.project.image.Image;
 import com.a101.ssafy.project.model.BasicResponse;
 import com.a101.ssafy.project.model.item.Item;
 import com.a101.ssafy.project.model.item.RegisterDto;
@@ -45,26 +46,41 @@ public class ItemController {
 		
 		Item item = new Item();
 		BeanUtils.copyProperties(request, item);
-		
+		System.out.println(request.toString()+"리퀘스트");
+		System.out.println(item.toString()+"아이템");
 		//위로 안 되는 것들 따로 설정(typeCasting->회의할것)
 		item.setName(request.getTitle());
-//		item.setStartPrice(Integer.parseInt(request.getStartPrice()));
-//		item.setHappyPrice(Integer.parseInt(request.getHappyPrice()));
+		item.setCategory("전자기기");
+		item.setStartPrice(Integer.parseInt(request.getStartPrice()));
+		item.setHappyPrice(Integer.parseInt(request.getHappyPrice()));
 //		item.setGrade(Integer.parseInt(request.getGrade()));
 //		item.setDirect(Integer.parseInt(request.getDirect()));
+		item.setDirect(1);
+		item.setGrade(2);
 //		item.setUserId(Integer.parseInt(request.getUserId()));
 		//이후 date 설정 해야함!
-		System.out.println(item.toString());
-		
-		itemService.registerItem(item);
+		System.out.println(item.toString()+"다 만든이후");
+
 		final BasicResponse result = new BasicResponse();
 		result.status = true;
 		result.data = "테스트";
 		result.object = null;
 		response = new ResponseEntity<>(result, HttpStatus.OK);
 		
-		for(int i=0; i<multipartFiles.length; ++i) {
-			String url = s3Service.upload(multipartFiles[i]);
+		if(multipartFiles!=null) {
+			for(int i=0; i<multipartFiles.length; ++i) {
+				String url = s3Service.upload(multipartFiles[i]);
+				Image image = new Image(item, url);
+				
+				item.addImage(image);
+			}			
+		}
+//		System.out.println(item.toString()+"냐옹");
+		if(itemService.registerItem(item)) {
+//			System.out.println(item.getId()+"ggg");
+			System.out.println("등록 성공");
+		}else {
+			System.out.println("등록 실패");
 		}
 		return response;
 	}
--- backend/src/main/java/com/a101/ssafy/project/image/UploadImageDto.java
+++
@@ -1,5 +0,0 @@
-package com.a101.ssafy.project.image;
-
-public class UploadImageDto {
-
-}
