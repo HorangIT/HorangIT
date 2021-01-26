636637aef78ee7fef54fa2f9ccd321f96123621b
sungjin <tjdwls3469@gmail.com>
2021-01-26 15:25:48 +0900

    [Feat] Update Login_BE And Signup_BE   - 비동기 응답시 user 정보 포함시켰습니다   - pom.xml에 json관련 디펜던시 추가했습니다

--- backend/src/main/java/com/a101/ssafy/project/service/UserServiceImpl.java
+++ backend/src/main/java/com/a101/ssafy/project/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.a101.ssafy.project.service;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,8 +17,8 @@ import com.a101.ssafy.project.model.BasicResponse;
 import com.a101.ssafy.project.model.user.Authority;
 import com.a101.ssafy.project.model.user.LoginDto;
 import com.a101.ssafy.project.model.user.SignupDto;
-import com.a101.ssafy.project.model.user.TokenDto;
 import com.a101.ssafy.project.model.user.User;
+import com.a101.ssafy.project.model.user.UserDto;
 
 import java.util.Collections;
 
@@ -37,7 +38,8 @@ public class UserServiceImpl {
 		this.authenticationManagerBuilder = authenticationManagerBuilder;
 	}
     
-    public Object login(LoginDto loginDto) {
+    @SuppressWarnings("unchecked")
+	public Object login(LoginDto loginDto) {
     	final BasicResponse result = new BasicResponse();
     	User user = userRepository.findByEmail(loginDto.getEmail());
     	
@@ -56,17 +58,21 @@ public class UserServiceImpl {
 		// 인증정보를 통해서 JWT 토큰생성
 		String jwt = tokenProvider.createToken(authentication);
 		
+		UserDto userDto = new UserDto(user.getId(), user.getEmail(), user.getUsername(), user.getNickname(), user.getAddress(), user.getPhone());
+		JSONObject jsonObject = new JSONObject();
+        jsonObject.put("token", jwt);
+        jsonObject.put("user", userDto);
+        
         result.status = true;
         result.data = "로그인 성공";
-        result.object = new TokenDto(jwt);
+        result.object = jsonObject;
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @Transactional
+    @SuppressWarnings("unchecked")
+	@Transactional
     public Object signup(SignupDto signupDto) {
     	final BasicResponse result = new BasicResponse();
-    	
-    	System.out.println(signupDto);
     	
     	if(userRepository.findByEmail(signupDto.getEmail()) != null) {
     		result.status = false;
@@ -88,8 +94,13 @@ public class UserServiceImpl {
         
         userRepository.save(user);
         
+		UserDto userDto = new UserDto(user.getId(), user.getEmail(), user.getUsername(), user.getNickname(), user.getAddress(), user.getPhone());
+		JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user", userDto);
+        
         result.status = true;
 		result.data = "회원가입 성공";
+        result.object = jsonObject;
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
