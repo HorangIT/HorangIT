a669246c602baf988014d2577cc3b9aa6b0c4101
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-01-26 10:46:58 +0900

    [feat] Image upload in s3

--- backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
@@ -1,15 +1,20 @@
 package com.a101.ssafy.project.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.a101.ssafy.project.model.BasicResponse;
 import com.a101.ssafy.project.model.item.Item;
@@ -20,7 +25,7 @@ import com.a101.ssafy.project.service.S3Service;
 
 @CrossOrigin(origins = { "*" })
 @Controller
-@RequestMapping("/item")
+@RequestMapping("/post")
 public class ItemController {
 	ItemService itemService;
 	
@@ -30,30 +35,26 @@ public class ItemController {
 	@Autowired
 	public void setItemService(ItemService itemService) {
 		this.itemService = itemService;
-	}
+	} 
 	
 	@PostMapping
-	public Object registerItem(@RequestBody RegisterDto request) {
-		System.out.println(request.toString());
+	public Object registerItem(RegisterDto request, @RequestParam("files") MultipartFile[] multipartFiles) throws IOException {
+		for(int i=0; i<multipartFiles.length; ++i) {
+			s3Service.upload(multipartFiles[i]);
+		}
 		
 		ResponseEntity response = null;
 		
 		Item item = new Item();
-		
 		BeanUtils.copyProperties(request, item);
 		
-		//String imgPath = s3Service.upload(file)
-		//file <- MultipartFile file
-		//galleryDto.setFilePath(imgPath);
-		
-		//galleryService.savePost(galleryDto);
 		//위로 안 되는 것들 따로 설정(typeCasting->회의할것)
 		item.setName(request.getTitle());
-		item.setStartPrice(Integer.parseInt(request.getStartPrice()));
-		item.setHappyPrice(Integer.parseInt(request.getHappyPrice()));
-		item.setGrade(Integer.parseInt(request.getGrade()));
-		item.setDirect(Integer.parseInt(request.getDirect()));
-		item.setUserId(Integer.parseInt(request.getUserId()));
+//		item.setStartPrice(Integer.parseInt(request.getStartPrice()));
+//		item.setHappyPrice(Integer.parseInt(request.getHappyPrice()));
+//		item.setGrade(Integer.parseInt(request.getGrade()));
+//		item.setDirect(Integer.parseInt(request.getDirect()));
+//		item.setUserId(Integer.parseInt(request.getUserId()));
 		//이후 date 설정 해야함!
 		System.out.println(item.toString());
 		
