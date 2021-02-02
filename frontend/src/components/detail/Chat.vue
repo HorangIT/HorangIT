1e9a21a26d212c7512f5180a2a0b89e5572988f5
powerticket <jwp0530@gmail.com>
2021-02-02 10:35:17 +0900

    [Chore] Add comments

--- frontend/src/components/detail/Chat.vue
+++ frontend/src/components/detail/Chat.vue
@@ -4,7 +4,7 @@
       Chat
       <!-- 자신의 메시지는 오른쪽 정렬, user 정보 확인 필요 -->
       <!-- 가로 길이 길어졌을 때, 글씨 깨지는 현상 고쳐야 함 -->
-      <v-card v-for="(chat, index) in chatLog" :key="index" :class="{ 'text-right ': chat.userId === userId }">
+      <div v-for="(chat, index) in chatLog" :key="index" :class="{ 'text-right ': chat.userId === userId }">
         <v-chip dark style="height: auto; white-space: normal;" class="pa-4 mb-2" :class="{ 'primary': chat.userId === userId }">
           <strong>{{ chat.nickname }}</strong>
           {{ chat.content }}
@@ -13,7 +13,7 @@
             {{ new Date() }}
           </sub>
         </v-chip>
-      </v-card>
+      </div>
     </div>
     <v-text-field v-model="chatInput" placeholder="" @keydown.enter="submit">
       <v-icon
@@ -51,10 +51,12 @@ export default {
     }
   },
   updated () {
+    // Always scroll down
     const scroll = this.$el.querySelector('.chatScroll')
     scroll.scrollTop = scroll.scrollHeight;
   },
   methods: {
+    // 채팅 submit 서버 socket에 emit
     submit (event) {
       event.preventDefault();
       if (this.chatInput) {
@@ -71,16 +73,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  /* body { margin: 0; padding-bottom: 3rem; font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif; }
-
-  .chat-form { background: rgba(0, 0, 0, 0.15); padding: 0.25rem; display: flex; height: 3rem; box-sizing: border-box; backdrop-filter: blur(10px); }
-  .chat-input { border: none; padding: 0 1rem; flex-grow: 1; border-radius: 2rem; margin: 0.25rem; }
-  .chat-input:focus { outline: none; }
-  .chat-form > button { background: #333; border: none; padding: 0 1rem; margin: 0.25rem; border-radius: 3px; outline: none; color: #fff; }
-
-  .chat-messages { list-style-type: none; margin: 0; padding: 0; }
-  .chat-messages > li { padding: 0.5rem 1rem; }
-  .chat-messages > li:nth-child(odd) { background: #efefef; } */
-</style>
