c178c8550a71cd286f0f2d90ca9dbc09aa0e41b9
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-02-04 13:22:45 +0900

    [Chore] Add L function for Redis List in RedisUtil

--- backend/src/main/java/com/a101/ssafy/project/redis/RedisUtil.java
+++ backend/src/main/java/com/a101/ssafy/project/redis/RedisUtil.java
@@ -1,10 +1,12 @@
 package com.a101.ssafy.project.redis;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -56,6 +58,22 @@ public class RedisUtil {
     public Object getHdata(String redisKey, String key) {
     	HashOperations<String, Object, Object> valueOperations = stringRedisTemplate.opsForHash();
     	return valueOperations.get(redisKey, key);
+    }
+    
+    /** redis List 구조 저장 및 조회를 위한 함수 */
+    public Long setLdata(String key, String value) {
+    	ListOperations<String, String> valueOperations = stringRedisTemplate.opsForList();
+    	return valueOperations.rightPush(key, value);
+    }
+    
+    public List<String> getAllLdata(String key){
+    	ListOperations<String, String> valueOperations = stringRedisTemplate.opsForList();
+    	return valueOperations.range(key, 0, getLSize(key));
+    }
+    
+    public Long getLSize(String key) {
+    	ListOperations<String, String> valueOperations = stringRedisTemplate.opsForList();
+    	return valueOperations.size(key);
     }
 
 }
