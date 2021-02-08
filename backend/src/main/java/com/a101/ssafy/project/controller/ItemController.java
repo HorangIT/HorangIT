618172491417f9db7fb56abb607a5fa5a35382c0
Jamie Lee <itzjamie96@gmail.com>
2021-02-08 15:22:41 +0900

    [Feat] Auction Pagination error! ^0^

--- backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
+++ backend/src/main/java/com/a101/ssafy/project/controller/ItemController.java
@@ -3,9 +3,17 @@ package com.a101.ssafy.project.controller;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,7 +27,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.a101.ssafy.project.model.BasicResponse;
+import com.a101.ssafy.project.model.item.Item;
 import com.a101.ssafy.project.model.item.RegisterDto;
+import com.a101.ssafy.project.repository.ItemRepository;
 import com.a101.ssafy.project.service.ItemService;
 import com.a101.ssafy.project.service.S3Service;
 
@@ -38,6 +48,10 @@ public class ItemController {
 	public void setItemService(ItemService itemService) {
 		this.itemService = itemService;
 	} 
+	
+	// pagination을 위한 itemRepo
+	@Autowired
+	ItemRepository itemRepository;
 	
 	//이후 주석 꼭 달기!
 	
@@ -65,6 +79,8 @@ public class ItemController {
 		
 		return response;
 	}
+	
+	
 	@PostMapping
 	public Object registerItem(RegisterDto request, @RequestParam("files") MultipartFile[] multipartFiles) throws IOException, ParseException {
 		BasicResponse result = itemService.registerItem(request, multipartFiles);
@@ -77,6 +93,44 @@ public class ItemController {
 		}
 
 		return response;
+	}
+	
+	
+	@GetMapping("/page")
+	public Object getAllItems(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue ="12") int size) {
+
+		try {
+			
+			PageRequest paging = PageRequest.of(page, size);
+			Page<Item> itemPage = itemRepository.findAll(paging);
+			
+			System.out.println(itemPage.toString());
+//			
+			List<Item> items = itemPage.getContent();
+			System.out.println(items.size());
+			System.out.println(items.get(0).getName());
+			
+			
+//			Page<Item> itemPage = itemRepository.findByCategory("전자기기", paging);
+//			
+//			System.out.println(items.toString());
+//						
+//			
+//			Map<String, Object> response = new HashMap<String, Object>();
+//			response.put("items", items);
+//			response.put("currentPage", itemPage.getNumber());
+//			response.put("totalPages", itemPage.getTotalPages());
+//			
+//			System.out.println(response.toString());
+			
+			return new ResponseEntity<>(itemPage, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+
 	}
 	
 }
