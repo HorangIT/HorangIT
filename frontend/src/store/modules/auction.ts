57adac5fd378eef3c456fec8031321e024f566c3
kaslock <kaslock@naver.com>
2021-02-09 15:41:44 +0900

    [Feat] Session storage

---
+++ frontend/src/store/modules/auction.ts
@@ -0,0 +1,15 @@
+export const auctionModule: Record<string, any> = {
+  namespaced: true,
+  state: {
+    page: sessionStorage.getItem("page") ? sessionStorage.getItem("page") : 1,
+    filters: sessionStorage.getItem("filters") ? sessionStorage.getItem("filters") : {},
+  },
+  mutations: {
+    SET_PAGE (state: any, page: any) {
+      state.page = page;
+    },
+    SET_FILTER (state: any, filters: any) {
+      state.filters = filters;
+    }
+  }
+}
