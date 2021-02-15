14a77cacc556d1fdb2f6b4a421047ab6d7d03f1a
Jamie Lee <itzjamie96@gmail.com>
2021-02-15 23:50:26 +0900

    [Chore] changed payment input to requestBody

--- backend/src/main/java/com/a101/ssafy/project/model/payment/PaymentDto.java
+++ backend/src/main/java/com/a101/ssafy/project/model/payment/PaymentDto.java
@@ -1,5 +1,26 @@
 package com.a101.ssafy.project.model.payment;
 
-public class PaymentDto {
+import javax.validation.Valid;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Valid
+@Setter
+@Getter
+@ToString
+public class PaymentDto {
+	
+	@ApiModelProperty(required = true)
+	String name;
+	
+	@ApiModelProperty(required = true)
+	Long price;
+	
+	@ApiModelProperty(required = true)
+	Long buyerId;
+	
+	
 }
