43d2497affb9f0291ed7cfb749dce46b019f8f97
powerticket <jwp0530@gmail.com>
2021-03-02 23:26:50 +0900

    Updates

---
+++ precache-manifest.72665abc5bef88b6d1a6b80d31adfade.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0f7c66f362385c61a4",
+    "url": "/css/app.8ddc31c2.css"
+  },
+  {
+    "revision": "17efe08386baf654c86d",
+    "url": "/css/chunk-vendors.a34fe6c5.css"
+  },
+  {
+    "revision": "bb373ff9e4f7ec46ff0a7d5a5e015a66",
+    "url": "/horangit.png"
+  },
+  {
+    "revision": "46459fff198dc254deda85d6ded95920",
+    "url": "/img/avatar_male.46459fff.svg"
+  },
+  {
+    "revision": "61f8186ae112a9b2d2cf70e6fc537d5f",
+    "url": "/img/horangit.61f8186a.png"
+  },
+  {
+    "revision": "67435140a8bcd7274a6d9976534a87a9",
+    "url": "/img/horangit_1.67435140.png"
+  },
+  {
+    "revision": "6dcda89d00d7df61c067b9371bed1a6e",
+    "url": "/img/horangit_4.6dcda89d.png"
+  },
+  {
+    "revision": "703920be289b64e2c7572cee844e623f",
+    "url": "/img/horangit_5.703920be.png"
+  },
+  {
+    "revision": "8e56197a7d8341a8538cc77191b77e05",
+    "url": "/img/horangit_6.8e56197a.png"
+  },
+  {
+    "revision": "dd4811ab9db1c093c9ed35ce1c4c34f7",
+    "url": "/img/horangit_face.dd4811ab.png"
+  },
+  {
+    "revision": "5600544809de8d76c918fa6d5abdaf74",
+    "url": "/img/main_buy.56005448.jpg"
+  },
+  {
+    "revision": "d4de5470a0a8cce87ffb4a0e35311e09",
+    "url": "/img/main_info.d4de5470.jpg"
+  },
+  {
+    "revision": "4a876f053f30848ccf218e8bb87343ed",
+    "url": "/img/main_sell.4a876f05.jpg"
+  },
+  {
+    "revision": "a2947e73040f93afed84af1c91998c5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd0f7c66f362385c61a4",
+    "url": "/js/app.77f59ec8.js"
+  },
+  {
+    "revision": "17efe08386baf654c86d",
+    "url": "/js/chunk-vendors.e339421f.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
